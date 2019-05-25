c699f4f11da78a73bfc0357f923797bab8c3b3c8
gun1x <gheorghe@linux.com>
2019-05-25 13:03:30 +0300

    added docker repo

--- .docker-build.sh
+++ .docker-build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 docker build -t registry.gitlab.com/gun1x/cyberghost-wireguard .
 docker push registry.gitlab.com/gun1x/cyberghost-wireguard
+docker tag registry.gitlab.com/gun1x/cyberghost-wireguard gunix/cyberghost-wireguard:latest
+docker push gunix/cyberghost-wireguard:latest
